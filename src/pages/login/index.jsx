e72de85705d80c4d9f2ce061c283859d1c7cb6f2
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-10-06 12:23:12 -0300

    A requisição par api está retonando erro 304.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,15 +5,15 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { api } from '../../services/api';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+//import { yupResolver } from '@hookform/resolvers/yup';
+//import * as yup from 'yup';
 import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, EntrarBottom, Wrapper } from './styles';
-
+/*
 const schema = yup.object({
         email: yup.string().email('O e-mail inserido não consta como usuário!').required(),
-        password: yup.number().required(),
+        senha: yup.number().required(),
     }).required();
-
+*/
 const Login = () => {
 
     const navigate = useNavigate()
@@ -24,15 +24,15 @@ const Login = () => {
         alert('Entre em contato com nossa equipe por: suporte@email.com')
     }
 
-    const { control, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema),
+const { control, handleSubmit, formState: { errors /*, isValid*/  } } = useForm({
+        reValidateMode: 'onChange',
         mode: 'onChange',
     });
- 
+    //const onSubmit = data => console.log(data)
     //console.log(errors, isValid)
     const onSubmit = async (formData) => {
         try{
-            const {data} = await api.get(`/users?email=${formData.email}&password=${formData.password}`);
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
             console.log(data);
 
             if(data.length && data[0].id){
@@ -41,13 +41,11 @@ const Login = () => {
             }
 
             alert('Usuário ou senha inválido')
-        }catch(e){
-            //TODO: HOUVE UM ERRO
+        }catch(errors){
+            //console.log('errors', errors);
         }
     };
-
-    console.log('errors', errors);
-
+    //console.log(isValid, errors);
     return (
     <>
         <Header />
@@ -62,7 +60,9 @@ const Login = () => {
                 <SubtitleLogin>Faça seu login e make the change.</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />} name="password" control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="senha" placeholder="Senha" leftIcon={<MdLock />} name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
                     <EntrarBottom>
                         <Button title="Entrar" variant="secondary" type="submit"/>    
                     </EntrarBottom> 
